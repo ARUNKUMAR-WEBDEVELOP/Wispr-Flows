c95c42d815ce64d3c111922033bf759a14cdcf41
arun <170822213+ARUNKUMAR-WEBDEVELOP@users.noreply.github.com>
2026-01-02 14:56:42 +0530

    Remove backend folder from master branch

--- wispr-backend/apps/account/urls.py
+++
@@ -1,8 +0,0 @@
-# apps/auth/urls.py
-from django.urls import path
-from .views import google_login, me
-
-urlpatterns = [
-    path("google/", google_login, name="google-login"),
-    path("me/", me, name="me"),
-]
